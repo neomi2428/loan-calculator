bd76da7ac1762536b1d689a8fbf549eb5be7c977
Junhee Park <neomi2428@gmail.com>
2017-07-28 01:04:18 -0700

    get a balance

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,15 @@
   <div id="app">
     <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
     <label for="home-price">Home price</label>
-    <input type="text" id="home-price" />
+    <input type="number" id="home-price" value="30000" />
     <label for="down-payment">Down payment</label>
-    <input type="text" id="down-payment" />
+    <input type="number" id="down-payment" value="6000" />
     <label for="interest-rate">Interest rate</label>
-    <input type="text" id="interest-rate" />
+    <input type="number" id="interest-rate" value="4" />
     <button v-on:click="calculate" type="submit">Calculate</button>
+    <div id="loan-result">
+    </div>
   </div>
 </template>
 
@@ -18,12 +19,40 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Loan Calculator'
+      msg: 'Simple Loan Calculator'
     }
   },
   methods: {
     calculate: function (event) {
-      console.log('calculate')
+      var homePrice = document.getElementById("home-price").value
+      var downPayment = document.getElementById("down-payment").value
+      var interestRate = document.getElementById("interest-rate").value
+      var loanAmount = this.getLoanAmount(homePrice, downPayment)
+
+
+      document.getElementById("loan-result").innerHTML = "loan-result"
+    },
+    getLoanAmount: function(homePrice, downPayment) {
+      homePrice = Number(homePrice)
+      if (typeof homePrice !== "number" || isNaN(homePrice)) {
+        console.log("A home price is not a number.")
+        return;
+      }
+      downPayment = Number(downPayment)
+      if (typeof downPayment !== "number" || isNaN(downPayment)) {
+        console.log("A down payment is not a number.")
+        return;
+      }
+      return homePrice - downPayment
+    },
+    getBalance: function (loanAmount, loanTermMonth, interestRate, monthlyPayment) {
+      var balance = loanAmount
+      for (var i = 0; i < loanTermMonth; ++i) {
+        // add interest
+        balance *= (1.0 + (interestRate / 12.0) / 100.0)
+        balance -= monthlyPayment
+      }
+      return balance
     }
   }
 }
